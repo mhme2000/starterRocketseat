745561e0a8d4ac3e6cc8e0ce3d2df2ad3f8bab51
marcos henrique <Marcos Eckart>
2020-01-23 09:14:14 -0400

    Finished

---
+++ ex01.js
@@ -0,0 +1,13 @@
+var endereco = {
+    rua: "Rua dos pinheiros",
+    numero: 1293,
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+   };
+
+function Relatorio(objetoEndereco){
+    return console.log(`O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua "${endereco.rua}" com nº ${endereco.numero} `)
+}
+
+Relatorio(endereco)
