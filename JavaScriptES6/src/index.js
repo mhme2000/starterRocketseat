0058414f06293fb8a7895cfe89d5593c30cad508
marcos henrique <Marcos Eckart>
2020-01-23 13:47:05 -0400

    add path await and async

--- JavaScriptES6/src/index.js
+++ JavaScriptES6/src/index.js
@@ -1,1 +1,18 @@
-alert('Test')
+const minhaPromisse = () => new Promise((resolve, reject) => {
+    setTimeout(() => {resolve('OK')}, 2000)
+})
+
+//minhaPromisse().then(response => {
+//    console.log(response)
+//})
+//.catch(err => {
+
+//})
+
+async function executaPromise(){
+    const response = await minhaPromisse()
+
+    console.log(response)
+}
+
+executaPromise()
