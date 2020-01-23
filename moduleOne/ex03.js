b894e5fa2381b08e522ce12887460b119a967000
marcos henrique <Marcos Eckart>
2020-01-23 09:30:32 -0400

    ex03 finished

---
+++ moduleOne/ex03.js
@@ -0,0 +1,8 @@
+function temHabilidade(skills) {
+    // código aqui
+   return (skills.indexOf("Javascript", [0]) == 0) ? true : false
+
+   }
+   var skills = ["Javascript", "ReactJS", "React Native"]
+  console.log(temHabilidade(skills)) // true ou false
+   
